b5cae8914e121133c56b53098c8bc81d4eca28e8
MVaishnavi27 <60087109+MVaishnavi27@users.noreply.github.com>
2020-02-04 16:13:31 +0530

    Add files via upload

---
+++ Regular_Employee.java
@@ -0,0 +1,30 @@
+package com;
+
+import javax.persistence.*;  
+  
+@Entity  
+@DiscriminatorValue("regularemployee")  
+public class Regular_Employee extends Employee{  
+      
+@Column(name="salary")    
+private float salary;  
+  
+@Column(name="bonus")     
+private int bonus;
+
+public float getSalary() {
+	return salary;
+}
+
+public void setSalary(float salary) {
+	this.salary = salary;
+}
+
+public int getBonus() {
+	return bonus;
+}
+
+public void setBonus(int bonus) {
+	this.bonus = bonus;
+}  
+}  
