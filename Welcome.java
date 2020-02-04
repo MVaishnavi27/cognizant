83053ad1676b67941a5dda3ff0e175731542492b
MVaishnavi27 <60087109+MVaishnavi27@users.noreply.github.com>
2020-02-04 16:10:01 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,18 @@
+package com;
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+public class Welcome extends HttpServlet {  
+  
+    public void doPost(HttpServletRequest request, HttpServletResponse response)  
+        throws ServletException, IOException {  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("userName");  
+    out.print("Welcome "+n);  
+    }  
+  
+}  
