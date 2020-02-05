bfea5ea0519efafe46d6d9f5aaec9c00918b81cf
MVaishnavi27 <60087109+MVaishnavi27@users.noreply.github.com>
2020-02-05 15:30:13 +0530

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,19 +1,20 @@
-import {NgModule}  from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {UserComponent} from '../user/user.component';
-import {LoginComponent} from '../login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+
+
 const routes: Routes = [
-  { path: 'user', component: UserComponent },
-  { path: 'login', component: LoginComponent },
-  {path : '', component : LoginComponent}
+{path:"app" , component:AppComponent},
+{path:"login", component:LoginComponent},
+{path:"register", component:RegisterComponent},
+{path:"about" , component:AboutComponent}
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
