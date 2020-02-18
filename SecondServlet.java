c713620b50fd4336a5523dad064d66058e9aa267
MVaishnavi27 <60087109+MVaishnavi27@users.noreply.github.com>
2020-02-18 14:53:25 +0530

    Add files via upload

---
+++ SecondServlet.java
@@ -0,0 +1,23 @@
+package com;
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;  
+  
+public class SecondServlet extends HttpServlet {  
+  
+public void doPost(HttpServletRequest request, HttpServletResponse response){  
+    try{  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+      
+    Cookie ck[]=request.getCookies();  
+    out.print("Hello "+ck[0].getValue());  
+  
+    out.close();  
+  
+         }catch(Exception e){System.out.println(e);}  
+    }  
+      
+  
+}  
