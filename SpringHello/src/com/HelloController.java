73ebd1340b58000ddb169c451c248a54ee1e53c1
MVaishnavi27 <60087109+MVaishnavi27@users.noreply.github.com>
2020-02-18 15:05:47 +0530

    Add files via upload

---
+++ SpringHello/src/com/HelloController.java
@@ -0,0 +1,11 @@
+package com; 
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+@Controller  
+public class HelloController {  
+@RequestMapping("/")  
+    public String display()  
+    {  
+        return "index";  
+    }     
+}  
