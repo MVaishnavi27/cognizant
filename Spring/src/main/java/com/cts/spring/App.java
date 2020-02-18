b800b29f7a223aff20b61f5fd901003908449c5f
MVaishnavi27 <60087109+MVaishnavi27@users.noreply.github.com>
2020-02-18 15:01:59 +0530

    Add files via upload

---
+++ Spring/src/main/java/com/cts/spring/App.java
@@ -0,0 +1,13 @@
+package com.cts.spring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Spring-Module.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+		obj.printHello();
+	}
+}
